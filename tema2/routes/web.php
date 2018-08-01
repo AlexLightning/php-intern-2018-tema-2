2371dbfed301ca0430d1ee57b7bf92f829a4bca2
AlexLightning <37123761+AlexLightning@users.noreply.github.com>
2018-08-01 11:17:31 +0300

    small update

--- tema2/routes/web.php
+++ tema2/routes/web.php
@@ -5,11 +5,11 @@ Route::get('/', 'CorpController@index');
 Route::get('/companies', 'CorpController@showPage');
 Route::get('/companies/create', 'CorpController@create');
 Route::post('/companies', 'CorpController@store');
-Route::patch('companies/{id}','CorpController@update');
+Route::put('companies/{id}','CorpController@update');
 Route::delete('companies/{id}','CorpController@destroy');
 
 Route::get('/employees', 'EmplController@showPage');
 Route::get('/employees/create', 'EmplController@create');
 Route::post('/employees', 'EmplController@store');
-Route::patch('employees/{id}','EmplController@update');
+Route::put('employees/{id}','EmplController@update');
 Route::delete('employees/{id}','EmplController@destroy');
